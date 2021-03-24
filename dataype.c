61ba9a69b0cfbe621cc0610e4f6e55f3f2e32093
Ipsita2k <74016429+Ipsita2k@users.noreply.github.com>
2021-03-24 20:58:27 +0530

    datatype.c
    program will  show the size of that data type

---
+++ dataype.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of int : "<<sizeof (int)<<"byte"<<endl;
+    cout<<"size if char : "<<sizeof (char)<<"bytes"<<endl;
+    cout<<"size of float : "<<sizeof (float)<<"bytes"<<endl;
+    cout<<"size of double : "<<sizeof (double)<<"bytes"<<endl;
+    return 0;
+    
+}
